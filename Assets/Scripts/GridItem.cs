3c598ea4df8c920f14edae40a96e04592671be95
LostDaemon <lost_daemon@hotmail.com>
2025-01-23 00:33:12 +0100

    Add tiles

--- Assets/Scripts/GridItem.cs
+++ Assets/Scripts/GridItem.cs
@@ -1,4 +1,5 @@
 using System;
+using TMPro;
 using UnityEngine;
 
 public class GridItem : MonoBehaviour, IInteractive
@@ -11,7 +12,8 @@ public class GridItem : MonoBehaviour, IInteractive
         set
         {
             _isSelected = value;
-            _renderer.material.color = _isSelected ? Color.white : ItemsTypeHelper.GetColorByItemType(ItemType);
+            //_renderer.material.color = _isSelected ? Color.white : ItemsTypeHelper.GetColorByItemType(ItemType);
+            _renderer.material.color = _isSelected ? Color.yellow : Color.white;
         }
     }
 
@@ -29,6 +31,7 @@ public class GridItem : MonoBehaviour, IInteractive
     {
         _renderer = this.GetComponent<Renderer>();
         this.name = ItemType.ToString() + " " + Guid.NewGuid().ToString();
-        _renderer.material.color = ItemsTypeHelper.GetColorByItemType(ItemType);
+        this.GetComponentInChildren<TextMeshPro>().text = ((int)ItemType).ToString();
+        //_renderer.material.color = ItemsTypeHelper.GetColorByItemType(ItemType);
     }
 }
